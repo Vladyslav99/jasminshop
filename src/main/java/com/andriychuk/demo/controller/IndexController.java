8661155d536c09221d5b6fe66a47fc7c59e9a437
Vladyslav Kondratenko <1999vlad1999kondratenko@gmail.com>
2021-06-01 22:07:52 +0300

    Add security

--- src/main/java/com/andriychuk/demo/controller/IndexController.java
+++ src/main/java/com/andriychuk/demo/controller/IndexController.java
@@ -1,12 +1,21 @@
 package com.andriychuk.demo.controller;
 
+import com.andriychuk.demo.entity.CustomUser;
+import com.andriychuk.demo.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @GetMapping(value = "/")
     public String getIndex() {
@@ -23,8 +32,26 @@ public class IndexController {
         return "login";
     }
 
+    @GetMapping(value = "/login-failure")
+    public String getLoginFailureMessage(Model model) {
+        model.addAttribute("error", true);
+        return "login";
+    }
+
     @GetMapping(value = "/personal")
     public String getPersonal() {
         return "personal";
+    }
+
+
+    @GetMapping("/create-admin")
+    public String createAdmin() {
+        CustomUser admin = CustomUser.builder()
+                .login("admin")
+                .password("11111")
+                .role(CustomUser.Role.ADMIN)
+                .build();
+        customUserDetailsService.save(admin);
+        return "login";
     }
 }
